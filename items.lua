7b376a75a45213932dd662ca8bcae9fd540025ca
Phantom <59034736+lilphantom25@users.noreply.github.com>
2022-02-26 02:01:16 +0000

    new items

---
+++ items.lua
@@ -0,0 +1,28 @@
+ 
+ 
+ 
+ 
+ 
+ 
+-- TOYS
+ 
+    ["toy-egg"] 			    = {["name"] = "toy-egg", 				["label"] = "Burgershot Toy Egg", 			["weight"] = 1000,  	["type"] = "item", 		["image"] = "toy-egg.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Gives Random Toys"},
+    ["toy-eggg"] 			    = {["name"] = "toy-eggg", 				["label"] = "RoosterRest Toy Egg", 			["weight"] = 1000,  	["type"] = "item", 		["image"] = "toy-eggg.png", 		["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Gives Random Toys"},
+    ["barry"] 			        = {["name"] = "barry", 					["label"] = "Barry", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "barry.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["lenny"] 			        = {["name"] = "lenny", 					["label"] = "Lenny", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "lenny.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["oki"] 			        = {["name"] = "oki", 					["label"] = "Oki", 			                ["weight"] = 1000,  	["type"] = "item", 		["image"] = "oki.png", 			    ["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["hanno"] 			        = {["name"] = "hanno", 					["label"] = "Hanno", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "hanno.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["kiki"] 			        = {["name"] = "kiki", 					["label"] = "Kiki", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "kiki.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["bjorn"] 			        = {["name"] = "bjorn", 					["label"] = "Bjorn", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "bjorn.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["buddha"] 			        = {["name"] = "buddha", 				["label"] = "Buddha", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "buddha.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["clayvon"] 			    = {["name"] = "clayvon", 				["label"] = "Clayvon", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "clayvon.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["donnie"] 			        = {["name"] = "donnie", 				["label"] = "Donnie", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "donnie.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["gloryon"] 			    = {["name"] = "gloryon", 				["label"] = "Gloryon", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "gloryon.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["lando"] 			        = {["name"] = "lando", 					["label"] = "Lando", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "lando.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["leyla"] 			        = {["name"] = "leyla", 					["label"] = "Leyla", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "leyla.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["meowfurryon"] 			= {["name"] = "meowfurryon", 			["label"] = "MeowFurryon", 		            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "meowfurryon.png", 		["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["stagdancer"] 			    = {["name"] = "stagdancer", 			["label"] = "StagDancer", 		            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "stagdancer.png", 		["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["sven"] 			        = {["name"] = "sven", 					["label"] = "Sven", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "sven.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+    ["yeager"] 			        = {["name"] = "yeager", 				["label"] = "Yeager", 			            ["weight"] = 1000,  	["type"] = "item", 		["image"] = "yeager.png", 			["unique"] = false,     ["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Toy"},
+
+
